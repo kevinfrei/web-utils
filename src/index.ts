49be7eef5d5fb1f20718d155ad96bec9dafec960
Kevin Frei <kevinfrei@hotmail.com>
2021-07-09 21:30:54 -0700

    I think it works now

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export { Dock, DockProps } from './Dock';
-export { Fill, FillProps } from './Fill';
-export { FullPage, FullPageProps } from './FullPage';
+export { Dock } from './Dock';
+export type { DockProps } from './Dock';
+export { Fill } from './Fill';
+export type { FillProps } from './Fill';
+export { FullPage } from './FullPage';
+export type { FullPageProps } from './FullPage';
